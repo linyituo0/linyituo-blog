42d784c7f98c4c9c2b4f735040787807b33cbab7
happyboy <103669586+happyboy2022@users.noreply.github.com>
2025-03-14 02:20:54 +0400

    update nuxt and wrangler configurations for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr: true,
   nitro: {
-    preset: 'cloudflare-pages'
+    preset: 'cloudflare-pages',
   },
   // 启用TypeScript
   typescript: {
