865b8977092808cb495c0b4301415196e5c6fcb6
happyboy <103669586+happyboy2022@users.noreply.github.com>
2025-03-14 01:53:08 +0400

    update nuxt configuration and modify movie entries in movies.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  
+  ssr: true,
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
   // 启用TypeScript
   typescript: {
     strict: true
